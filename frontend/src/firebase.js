722b487225f218f242334322a41a95fefcc3e409
Sharadhi Hegde <73776648+sharadhih@users.noreply.github.com>
2024-11-29 22:08:45 +0530

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB9zu42ViuExi1qpKjmXOR5nsi9Uc9w6AI",
-  authDomain: "promentorai-37f12.firebaseapp.com",
-  projectId: "promentorai-37f12",
-  storageBucket: "promentorai-37f12.firebasestorage.app",
-  messagingSenderId: "376916122388",
-  appId: "1:376916122388:web:36ee2db922e100992ec001",
-  measurementId: "G-PXYJJ6GMM0"
+ //api keys
 };
 
 // Initialize Firebase
